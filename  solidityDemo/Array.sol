7f57d4f379621863d8f8eacc7150c05834587bd4
qing <ikin@bitkan.com>
2022-06-07 23:08:25 +0800

    add

---  solidityDemo/Array.sol
+++  solidityDemo/Array.sol
@@ -24,4 +24,29 @@ contract Array {
     function returnArray() external view returns (uint[] memory) {
         return nums;
     }
+
+    //移除一个元素和它的索引
+    function remove(uint _index) public { //因为delete会留默认，所以自己写
+        require(_index < nums.length, "length err");
+        for (uint i = _index; i < nums.length -1; i++) {
+            nums[i] = nums[i+1];
+        }
+        nums.pop();
+    }
+
+    //如果对顺序没要求：可以用数组最后一个元素替换要删除的那个元素 最后再pop就好了 -- 这个方法比上一个方法节约gas
+    function remove2(uint _index) public {
+        uint numLength = nums.length -1;
+        if (numLength > 0) {
+            uint lastNum = nums[nums.length -1];
+            nums[_index] = lastNum;
+            nums.pop();
+        }
+    }
+
+    function test() external {
+        remove2(1);
+        assert(nums.length == 2);
+    }
+
 }
